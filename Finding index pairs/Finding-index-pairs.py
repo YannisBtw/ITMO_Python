f3e7c30f8a6eb25cf9a1565453eaee22dea90fa8
YannisBtw <151903591+YannisBtw@users.noreply.github.com>
2025-09-05 13:04:39 +0300

    Add files via upload

---
+++ Finding index pairs/Finding-index-pairs.py
@@ -0,0 +1,24 @@
+def summ(nums, target):
+    ans = []
+
+    if len(nums) < 2:
+        return None
+
+    for j in range(len(nums) - 1):
+        for i in range(j + 1, len(nums)):
+            if nums[j] + nums[i] == target:
+                ans.append((j, i))
+
+    if not ans:
+        return None
+
+    return min(ans)
+
+
+try:
+    data_1 = list(map(int, input('Введите массив чисел через пробел: ').split()))
+    data_2 = int(input('Введите число для поиска: '))
+    print(summ(data_1, data_2))
+
+except ValueError:
+    print("Ошибка: вводите только целые числа через пробел!")
